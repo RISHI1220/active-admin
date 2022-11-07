fe81d2869346c3e94a0c135141ad3f800551ff8a
Rishi <Rishibarua08@gmail.com>
2022-11-07 15:09:32 +0530

    validations checks

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :user, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
   scope :published, -> { where.not(:published_at => nil)}
   scope :unpublished, -> { where(:published_at => nil)}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true, format: Devise.email_regexp, uniqueness: true
 end
